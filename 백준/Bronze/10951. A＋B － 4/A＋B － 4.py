6008ccd41b8404a6ff44b839f4b948e3a156a725
mountain-kangkang <96615456+mountain-kangkang@users.noreply.github.com>
2024-10-03 23:38:11 +0900

    Update A＋B － 4.py
    백준에서 ValueError가 발생했었음

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -1,6 +1,6 @@
 while True:
     try:
-        a, b = map(int, input().split())
+        a, b = map(int, input().split())        # 여기서 sys.stdin.readline()을 사용하면 KeyboardInterrupt가 발생하며 백준에서는 런타임에러(ValueError)가 발생한다
     except EOFError:
         break
     print(a + b)
