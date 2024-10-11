dbe6128240e125d4f7f56b65c4744d36197f191b
mountain-kangkang <96615456+mountain-kangkang@users.noreply.github.com>
2024-10-11 21:23:53 +0900

    [Silver I] Title: 영역 구하기, Time: 36 ms, Memory: 31632 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -0,0 +1,45 @@
+# https://acmicpc.net/problem/2583
+import sys
+
+def solution():
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    m, n, k = map(int, sys.stdin.readline().split())
+    land = [[1] * n for _ in range(m)]
+
+    territory = set()
+    for _ in range(k):
+        x1, y1, x2, y2 = map(int, sys. stdin. readline().split())
+        for x in range(x1, x2):
+            for y in range(y1, y2):
+                land[y][x] = 0
+    for x in range(n):
+        for y in range(m):
+            if land[y][x]:
+                territory.add((x, y))
+    
+    count = 0
+    areas = []
+
+    while territory:
+        spot = [territory.pop()]
+        area = 0
+        
+        while spot:
+            x, y = spot.pop()
+            area += 1
+
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+
+                if (nx, ny) in territory:
+                    territory.discard((nx, ny))
+                    spot.append ((nx, ny))
+        count += 1
+        areas.append(area)
+    sys.stdout.write(str(count)+'\n')
+    areas.sort()
+    [print(area, end=" ") for area in areas]
+    print()
+
+solution ()
