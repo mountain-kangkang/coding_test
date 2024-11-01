c96c8244dd313d468b84353687b5da5619e3143a
mountain-kangkang <96615456+mountain-kangkang@users.noreply.github.com>
2024-11-02 02:59:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 51.75 ms, Memory: 34 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,11 @@
+def solution(participant, completion):
+    part = {}
+
+    for p in participant:
+        if p in part : part[p] += 1
+        else : part[p] = 1
+
+    for c in completion:
+        part[c] -= 1
+
+    return ''.join([name for name in part if part[name] == 1])
