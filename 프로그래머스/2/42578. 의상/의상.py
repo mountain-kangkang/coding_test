cdf0a1bb288d52524c6fb7ea374de99d88daceca
KangKang <96615456+mountain-kangkang@users.noreply.github.com>
2025-03-05 21:18:31 +0900

    Update 의상.py
    문제 풀이 주석 추가

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -9,6 +9,6 @@ def solution(clothes):
 
     result = 1
     for category in clothes_dict:
-        result *= (len(clothes_dict[category]) + 1)
+        result *= (len(clothes_dict[category]) + 1)    # 각 카테고리에서 의상을 선택하지 않는 경우 고려하여 + 1
 
-    return result - 1
+    return result - 1    # 하지만 아무것도 입지 않는 경우는 없기 때문에 - 1
