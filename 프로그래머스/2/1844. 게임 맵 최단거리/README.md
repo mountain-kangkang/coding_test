3c9e8a4ab3b15d11bca2c96a4761b6384274ec07
KangKang <96615456+mountain-kangkang@users.noreply.github.com>
2025-03-17 20:26:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.34 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 11.84 ms
+메모리: 9.34 MB, 시간: 10.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 01일 00:38:49
+2025년 03월 17일 20:26:39
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,22 +1,47 @@
 from collections import deque
 
+# def solution(maps):
+#     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+#     queue = deque([(0, 0)])
+#     m, n = len(maps[0]), len(maps)
+#     distances = [[-1] * m for _ in range(n)]  # 거리 배열 초기화
+#     distances[0][0] = 1  # 시작점까지의 거리는 1
+
+#     while queue:
+#         x, y = queue.popleft()
+
+#         for dx, dy in directions:
+#             nx, ny = x + dx, y + dy
+#             if 0 <= nx < m and 0 <= ny < n and maps[ny][nx] == 1:
+#                 if distances[ny][nx] == -1:  # 방문하지 않은 경우
+#                     distances[ny][nx] = distances[y][x] + 1
+#                     queue.append((nx, ny))
+#                     if nx == m - 1 and ny == n - 1:  # 도착점에 도달한 경우
+#                         return distances[ny][nx]
+
+#     return -1  # 도착할 수 없는 경우
+
 def solution(maps):
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     queue = deque([(0, 0)])
-    m, n = len(maps[0]), len(maps)
-    distances = [[-1] * m for _ in range(n)]  # 거리 배열 초기화
-    distances[0][0] = 1  # 시작점까지의 거리는 1
+    sero, garo = len(maps), len(maps[0])
+    location = [[-1] * garo for _ in range(sero)]
+    location[0][0] = 1
 
     while queue:
-        x, y = queue.popleft()
+        current_location_sero, current_location_garo = queue.popleft()
 
-        for dx, dy in directions:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < m and 0 <= ny < n and maps[ny][nx] == 1:
-                if distances[ny][nx] == -1:  # 방문하지 않은 경우
-                    distances[ny][nx] = distances[y][x] + 1
-                    queue.append((nx, ny))
-                    if nx == m - 1 and ny == n - 1:  # 도착점에 도달한 경우
-                        return distances[ny][nx]
+        for direction_sero, direction_garo in direction:
+            next_location_sero, next_location_garo = current_location_sero + direction_sero, current_location_garo + direction_garo
 
-    return -1  # 도착할 수 없는 경우
+            if 0 <= next_location_sero < sero and 0 <= next_location_garo < garo:   # map의 범위를 벗어나는지 확인
+                if maps[next_location_sero][next_location_garo] == 1:               # 확인할 좌표가 갈 수 있는 길인지 확인
+                    if location[next_location_sero][next_location_garo] == -1:      # 아직 방문하지 않은 좌표인지 확인
+                        location[next_location_sero][next_location_garo] = location[current_location_sero][current_location_garo] + 1   # 현재 좌표에서 +1 하여 저장해서 거리 계산
+
+                        if next_location_sero == sero - 1 and next_location_garo == garo - 1:   # 만약 다음 좌표가 목표 지점이라면 그 거리를 반환하며 함수 종료
+                            return location[next_location_sero][next_location_garo]
+
+                        queue.append((next_location_sero, next_location_garo))
+
+    return -1
